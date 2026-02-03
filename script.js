fa3923b0beb5cc038dc005c8f24cc4ea7278641f
itzpuchu <michaelismaelcf@gmail.com>
2026-02-03 08:42:42 -0600

    Update and rename script.js to bans.html

--- script.js
+++
@@ -1,7 +0,0 @@
-// Login form
-function login(e) {
-  e.preventDefault();
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  alert(`Login attempt: ${email}`);
-}
